3a5902c57c2c8e11455138808a82e4032f3ffd03
mydnac <mandycoelho5@gmail.com>
2020-07-22 14:17:02 +0200

    feat(writer):add method getAll, getById, create, udpate and delete

--- src/main/java/com/ckp4/myPersonalLibraryapi/webservices/book/BookController.java
+++ src/main/java/com/ckp4/myPersonalLibraryapi/webservices/book/BookController.java
@@ -1,4 +1,44 @@
 package com.ckp4.myPersonalLibraryapi.webservices.book;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+@CrossOrigin
 public class BookController {
+
+    @Autowired
+    BookService service;
+
+    @GetMapping()
+    public List<Book> getAll() {
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable Long id){
+        return service.getBookById(id);
+    }
+
+    @PostMapping()
+    public Book createBook(@RequestBody Book book){
+        return service.createBook(book);
+    }
+
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable Long id, @RequestBody Book book){
+        return service.updateBook(id, book);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id){
+        service.deleteBook(id);
+    }
+
 }
+
--- src/main/java/com/ckp4/myPersonalLibraryapi/webservices/book/BookService.java
+++ src/main/java/com/ckp4/myPersonalLibraryapi/webservices/book/BookService.java
@@ -1,4 +1,40 @@
 package com.ckp4.myPersonalLibraryapi.webservices.book;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BookService {
+
+    @Autowired
+    BookRepository repository;
+
+    public List<Book> getAll() {
+        return repository.findAll();
+    }
+
+    public Book getBookById(Long id){
+        return repository.findById(id).get();
+    }
+
+    public Book createBook(Book book){
+        return repository.save(book);
+    }
+
+    public Book updateBook(Long id, Book book){
+        Book bookToUpdate = repository.findById(id).get();
+        bookToUpdate.setTitle(book.getTitle());
+        bookToUpdate.setRating(book.getRating());
+        bookToUpdate.setImg(book.getImg());
+        bookToUpdate.setReadingDate(book.getReadingDate());
+        bookToUpdate.setSummary(book.getSummary());
+        bookToUpdate.setPersonalSummary(book.getPersonalSummary());
+        return repository.save(bookToUpdate);
+    }
+
+    public void deleteBook(Long id){
+        repository.deleteById(id);
+    }
 }
