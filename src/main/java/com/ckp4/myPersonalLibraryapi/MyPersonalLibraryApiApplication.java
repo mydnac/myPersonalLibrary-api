15975c26d1af4ce7149da118494d31fce79e3882
mydnac <mandycoelho5@gmail.com>
2020-07-22 11:05:16 +0200

    feat(general):init project

---
+++ src/main/java/com/ckp4/myPersonalLibraryapi/MyPersonalLibraryApiApplication.java
@@ -0,0 +1,13 @@
+package com.ckp4.myPersonalLibraryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPersonalLibraryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPersonalLibraryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ckp4/myPersonalLibraryapi/MyPersonalLibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ckp4.myPersonalLibraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPersonalLibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
