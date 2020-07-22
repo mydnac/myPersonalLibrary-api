993b0a7abfafece0aff0fdd31c9ca89cc859b076
mydnac <mandycoelho5@gmail.com>
2020-07-22 14:16:35 +0200

    feat(writer):add method getById, create, udpate and delete

--- src/main/java/com/ckp4/myPersonalLibraryapi/webservices/writer/WriterController.java
+++ src/main/java/com/ckp4/myPersonalLibraryapi/webservices/writer/WriterController.java
@@ -1,4 +1,36 @@
 package com.ckp4.myPersonalLibraryapi.webservices.writer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/writers")
+@CrossOrigin
 public class WriterController {
+
+    @Autowired
+    WriterService service;
+
+    @GetMapping("/{id}")
+    public Writer getWriterById(@PathVariable Long id){
+        return service.getWriterById(id);
+    }
+
+    @PostMapping()
+    public Writer createWriter(@RequestBody Writer writer){
+        return service.createWriter(writer);
+    }
+
+    @PutMapping("/{id}")
+    public Writer updateWriter(@PathVariable Long id, @RequestBody Writer writer){
+        return service.updateWriter(id, writer);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id){
+        service.deleteWriter(id);
+    }
+
 }
--- src/main/java/com/ckp4/myPersonalLibraryapi/webservices/writer/WriterService.java
+++ src/main/java/com/ckp4/myPersonalLibraryapi/webservices/writer/WriterService.java
@@ -1,4 +1,30 @@
 package com.ckp4.myPersonalLibraryapi.webservices.writer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class WriterService {
+
+    @Autowired WriterRepository repository;
+
+    public Writer getWriterById(Long id){
+        return repository.findById(id).get();
+    }
+
+    public Writer createWriter(Writer writer){
+        return repository.save(writer);
+    }
+
+    public Writer updateWriter(Long id, Writer writer){
+        Writer writerToUpdate = repository.findById(id).get();
+        writerToUpdate.setLastname(writer.getLastname());
+        writerToUpdate.setFirstname(writer.getFirstname());
+        return repository.save(writerToUpdate);
+    }
+
+    public void deleteWriter(Long id){
+        repository.deleteById(id);
+    }
 }
